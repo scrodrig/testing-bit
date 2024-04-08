ad368c71befe550feb0bf307333533ada1038b7a
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-08 17:11:46 +0100

    fixes testing

--- testing/button_ui/button_ui.spec.tsx
+++ testing/button_ui/button_ui.spec.tsx
@@ -4,6 +4,6 @@ import { BasicButtonUi } from './button_ui.composition.js';
 
 it('should render the correct text', () => {
   const { getByText } = render(<BasicButtonUi />);
-  const rendered = getByText('hello world!');
+  const rendered = getByText('Click me');
   expect(rendered).toBeTruthy();
 });
